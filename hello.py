fb6ff888e185aac45befc9f606d95d48b57968b6
kuangfio <kuangfionaly@gmail.com>
2024-09-25 00:57:19 -0400

    Added new template that uses forms to get name and email

--- hello.py
+++ hello.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Regexp
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SeventeenPinkPineapples'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", name="Fiona", current_time=datetime.now(tz=timezone.utc))
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template("form_index.html", name=session.get('name'), form=form, email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField("What is your UofT Email address?", validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+#, Regexp('.*utoronto.*')
