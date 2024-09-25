9e31dea8a15c08695095ba9baf253e61357110a4
kuangfio <kuangfionaly@gmail.com>
2024-09-25 00:02:00 -0400

    Added index template and changed route to return rendered template

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template("index.html", name="Fiona", current_time=datetime.now(tz=timezone.utc))
 
 @app.route('/user/<name>')
 def user(name):
